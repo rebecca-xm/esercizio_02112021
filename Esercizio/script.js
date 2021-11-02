1c89b3657089c8d69434223264918d090c9dd0ab
Rebecca Mollica <rebecca.mollica@live.it>
2021-11-02 15:23:54 +0100

    esercizio con nuovo array

---
+++ Esercizio/script.js
@@ -0,0 +1,84 @@
+// Esercizi:
+// Obbligatorio: dato il seguente array di oggetti
+
+const data = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz"
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv"
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      username: "Samantha",
+      email: "Nathan@yesenia.net"
+    },
+    {
+      id: 4,
+      name: "Patricia Lebsack",
+      username: "Karianne",
+      email: "Julianne.OConner@kory.org"
+    },
+    {
+      id: 5,
+      name: "Chelsey Dietrich",
+      username: "Kamren",
+      email: "Lucio_Hettinger@annie.ca"
+    },
+    {
+      id: 6,
+      name: "Mrs. Dennis Schulist",
+      username: "Leopoldo_Corkery",
+      email: "Karley_Dach@jasper.info"
+    },
+    {
+      id: 7,
+      name: "Kurtis Weissnat",
+      username: "Elwyn.Skiles",
+      email: "Telly.Hoeger@billy.biz"
+    },
+    {
+      id: 8,
+      name: "Nicholas Runolfsdottir V",
+      username: "Maxime_Nienow",
+      email: "Sherwood@rosamond.me"
+    },
+    {
+      id: 9,
+      name: "Glenna Reichert",
+      username: "Delphine",
+      email: "Chaim_McDermott@dana.io"
+    },
+    {
+      id: 10,
+      name: "Clementina DuBuque",
+      username: "Moriah.Stanton",
+      email: "Rey.Padberg@karina.biz"
+    }
+  ]
+
+// utilizzare il metodo .forEach() per creare un array o sovrascrivere lo stesso, 
+// che contenga solamente i campi name ed email, 
+// quest'ultimo dovrà contenere testi esclusivamente in minuscolo.
+
+console.log(data);
+
+
+const dataTwo = [];
+  
+data.forEach(element => {
+    let arrayData = {
+        name: element.name.toLocaleLowerCase(),
+        email: element.email.toLocaleLowerCase(),
+    }
+    dataTwo.push(arrayData);
+});
+
+console.log(dataTwo);
